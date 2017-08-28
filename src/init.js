ab4099bdde4be49ed75bd85eeeecc20942989bde
Hack Reactor Student <student@hackreactor.com>
2017-08-28 15:44:26 -0700

    Call setPosition method on newly created dancer object

--- src/init.js
+++ src/init.js
@@ -15,18 +15,22 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    var dancerMakerFunctionName = $(this).data('makeDancer');
+    //console.log(dancerMakerFunctionName);
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancerMakerFunction = window['makeDancer'];
 
     // make a dancer with a random position
+    
+    console.log($('body').height());
 
-    var dancer = dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+    var dancer = new dancerMakerFunction(
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
+    dancer.setPosition();
     $('body').append(dancer.$node);
   });
 });
