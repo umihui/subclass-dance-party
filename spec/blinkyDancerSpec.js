2273e75d96b3889312a41bf00e986f216659549f
Hack Reactor Student <student@hackreactor.com>
2017-08-28 20:44:37 -0700

    complete blinky dancer

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -23,7 +23,6 @@ describe('blinkyDancer', function() {
       sinon.spy(blinkyDancer, 'step');
       expect(blinkyDancer.step.callCount).to.be.equal(0);
       clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
-      clock.tick(timeBetweenSteps);
 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -3,19 +3,22 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  console.log('Make Blinky dancer', top, left);
+  this.oldStep(timeBetweenSteps);
 
-   
 };
 
-makeBlinkyDancer.prototype.oldStep = function() {
-  this.step;
-};
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-makeBlinkyDancer.prototype.step = function() {
+makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
+
+makeBlinkyDancer.prototype.step = function(timeBetweenSteps) {
     // call the old version of step at the beginning of any call to this new version of step
-  this.oldStep();
+  this.$node.toggle();
+  this.oldStep(timeBetweenSteps);
+  
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
 };
