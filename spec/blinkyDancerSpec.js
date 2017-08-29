74397fa65a2aa1a258061f203ea14deb1bc4490c
Hack Reactor Student <student@hackreactor.com>
2017-08-29 12:14:31 -0700

    Update spec tests for two new dancer classes

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -31,3 +31,75 @@ describe('blinkyDancer', function() {
     });
   });
 });
+
+
+describe('popDancer', function() {
+
+  var popDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    popDancer = new makePopDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(popDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a step function that makes its node fade in and out', function() {
+    sinon.spy(popDancer.$node, 'fadeIn');
+    popDancer.step();
+    expect(popDancer.$node.fadeIn.called).to.be.true;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(popDancer, 'step');
+      expect(popDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+      expect(popDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(popDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
+
+
+
+
+describe('brownDancer', function() {
+
+  var brownDancer, clock;
+  var timeBetweenSteps = 100;
+
+  beforeEach(function() {
+    clock = sinon.useFakeTimers();
+    brownDancer = new makeBrownDancer(10, 20, timeBetweenSteps);
+  });
+
+  it('should have a jQuery $node object', function() {
+    expect(brownDancer.$node).to.be.an.instanceof(jQuery);
+  });
+
+  it('should have a step function that makes its node dance', function() {
+    sinon.spy(brownDancer, 'step');
+    brownDancer.step();
+    expect(brownDancer.step.called).to.be.true;
+  });
+
+  describe('dance', function() {
+    it('should call step at least once per second', function() {
+      sinon.spy(brownDancer, 'step');
+      expect(brownDancer.step.callCount).to.be.equal(0);
+      clock.tick(timeBetweenSteps); // ? it seems an extra tick is necessary...
+      clock.tick(timeBetweenSteps);
+      expect(brownDancer.step.callCount).to.be.equal(1);
+
+      clock.tick(timeBetweenSteps);
+      expect(brownDancer.step.callCount).to.be.equal(2);
+    });
+  });
+});
