ac343a9e4a5dec1fbb8d9458d33832ab1793336b
Hack Reactor Student <student@hackreactor.com>
2017-08-29 19:21:12 -0700

    add .dancer class to makeBrownDancer

--- src/brownDancer.js
+++ src/brownDancer.js
@@ -2,7 +2,7 @@ var makeBrownDancer = function(top, left, timeBetweenSteps) {
 
   makeDancer.call(this, top, left, timeBetweenSteps);
   
-  this.$node = $('<img src="src/polarBear.gif" alt = "" height="225" width="auto" style="position:absolute; border-radius: 75px;">');
+  this.$node = $('<img class = "dancer" src="src/polarBear.gif" alt = "" height="225" width="auto" style="position:absolute; border-radius: 75px;">');
   this.setPosition.call(this, top, left);
   this.step();
   
