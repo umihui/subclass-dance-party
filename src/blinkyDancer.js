519975a7de71a4b82e0688132484b9f71e316a07
Hack Reactor Student <student@hackreactor.com>
2017-08-29 19:20:06 -0700

    Refactor blinkyDancer to move back and forth

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -3,8 +3,12 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+  this.$node = $('<img class="dancer" src="src/star.png" alt = "" height="40" width="auto" style="position:absolute;">');
+  this.left = left;
+  this.top = top;
   console.log('Make Blinky dancer', top, left);
   this.step();
+  this.direction = true;
 
 };
 
@@ -15,7 +19,23 @@ makeBlinkyDancer.prototype.oldStep = makeDancer.prototype.step;
 
 makeBlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-  this.$node.toggle();
+  //console.log(this);
+  var top = this.top;
+  var left = this.left;
+  if (this.direction && left < $('body').width()) { 
+    this.left += 150;
+  } else if (this.direction && left >= $('body').width()) {
+    this.direction = false;
+  } else if (!this.direction && left > 0) {
+    this.left -= 150;
+  } else if (!this.direction && left < 0) {
+    this.direction = true;
+  }
+  var newPosition = {
+    top: this.top,
+    left: this.left
+  };
+  this.$node.animate(newPosition);
   this.oldStep();
   
     // toggle() is a jQuery method to show/hide the <span> tag.
